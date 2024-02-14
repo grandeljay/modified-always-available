61a845e6b7e3ba66da92993c24faaa50e96c2178
grandeljay <github@grandels.email>
2024-02-14 12:06:36 +0100

    chore(module): update to v0.1.1

--- src-mmlc/Constants.php
+++ src-mmlc/Constants.php
@@ -14,7 +14,7 @@ class Constants
     public const MODULE_CATEGORIES_NAME    = 'MODULE_CATEGORIES_GRANDELJAY_ALWAYS_AVAILABLE_CATEGORIES';
     public const MODULE_PRODUCT_NAME       = 'MODULE_PRODUCT_GRANDELJAY_ALWAYS_AVAILABLE_PRODUCT';
     public const MODULE_SHOPPING_CART_NAME = 'MODULE_SHOPPING_CART_GRANDELJAY_ALWAYS_AVAILABLE_SHOPPING_CART';
-    public const MODULE_VERSION            = '0.1.0';
+    public const MODULE_VERSION            = '0.1.1';
 
     public const TABLE_ALWAYS_AVAILABLE = 'grandeljay_always_available';
 }
